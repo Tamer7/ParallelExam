fb1c7d44017fa04b05ce09d8196be2c054ce97bb
Tamer <tamerjar@hotmail.com>
2021-01-23 14:17:20 +0200

    Add files via upload

---
+++ program.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class program {
+
+        // Linear-search function to find the index of an element
+        public static int findIndex(int arr[], int t)
+        {
+            // if array is Null
+            if (arr == null) {
+                return -1;
+
+            }
+
+            // find length of array
+            int len = arr.length;
+            int i = 0;
+
+            // traverse in the array
+            while (i < len) {
+                if (arr[i] > t) {
+                    return i;
+                }
+                else {
+                    i = i + 1;
+                }
+            }
+            return -1;
+        }
+
+        // Driver Code
+        public static void main(String[] args)
+        {
+            int[] my_array = { 5, 4, 6, 1, 3, 2, 7, 8, 9 };
+
+            int i = findIndex(my_array, 7);
+            System.out.println("Highest number"
+                    + my_array[i]);
+        }
+    }
+
