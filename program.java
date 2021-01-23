04a3e60dfc96100845c10be1dd2181d6a0eb7b40
Tamer <tamerjar@hotmail.com>
2021-01-23 18:00:13 +0200

    Update program.java

--- program.java
+++ program.java
@@ -1,40 +1,93 @@
-import java.util.*;
+package CalculateMax.PrimeNumbers;
 
-public class program {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 
-        // Linear-search function to find the index of an element
-        public static int findIndex(int arr[], int t)
-        {
-            // if array is Null
-            if (arr == null) {
-                return -1;
-
-            }
-
-            // find length of array
-            int len = arr.length;
-            int i = 0;
-
-            // traverse in the array
-            while (i < len) {
-                if (arr[i] > t) {
-                    return i;
-                }
-                else {
-                    i = i + 1;
-                }
-            }
+public class BerSuBall {
+    public static int findIndex(int[] arr, int t) throws ExecutionException, InterruptedException {
+        if (arr == null) {
             return -1;
         }
 
-        // Driver Code
-        public static void main(String[] args)
-        {
-            int[] my_array = { 5, 4, 6, 1, 3, 2, 7, 8, 9 };
+        int numberOfThreads = 4;
+        return findIndexInParallel(arr, t, numberOfThreads);
+    }
 
-            int i = findIndex(my_array, 7);
-            System.out.println("Highest number"
-                    + my_array[i]);
+    private static int findIndexInParallel(int[] arr, int t, int threadCount) throws ExecutionException, InterruptedException {
+        if(threadCount > arr.length) {
+            threadCount = arr.length;
+        }
+
+        int startIndex = 0;
+        int range = (int) Math.ceil(arr.length / (double) threadCount);
+        int endIndex = range;
+
+        // step 1: create threads using Executor FrameWork
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        List<Future<Integer>> futures = new ArrayList<>();
+        for (int i = 0; i < threadCount; i++) {
+
+            RangeFinder rangeFinder = new RangeFinder(Arrays.copyOfRange(arr, startIndex, endIndex), startIndex, t);
+
+            // Step 3: submit the task to thread pool
+            Future<Integer> submit = executorService.submit(rangeFinder);
+            startIndex = startIndex + range;
+            int newEndIndex = endIndex + range;
+            endIndex = Math.min(newEndIndex, arr.length);
+            futures.add(submit);
+        }
+
+        // Evaluating the test result
+        for (Future<Integer> future : futures) {
+            int index = (int) future.get();
+            if(index != -1) {
+                executorService.shutdownNow();
+                return index;
+            }
+        }
+        executorService.shutdown();
+        return -1;
+    }
+
+    static class RangeFinder implements Callable<Integer> {
+        private final int[] arr;
+        private final int startIndex;
+        private final int t;
+
+        RangeFinder(int[] arr, int startIndex, int t) {
+            this.arr = arr;
+            this.startIndex = startIndex;
+            this.t = t;
+        }
+
+
+        @Override
+        public Integer call() {
+            long start = System.nanoTime();
+            for (int i = 0; i < this.arr.length; i++) {
+                if (arr[i] > t) {
+                    return startIndex + i;
+                }
+            }
+            long end = System.nanoTime();
+            long elapsedTime = end - start;
+            double seconds = (double)elapsedTime / 1_000_000_000.0;
+            System.out.println("Thread : "+Thread.currentThread().getId() + "  "+seconds);
+            return -1;
         }
     }
+
+    // Driver Code - Finds the biggest number of in the array after the constant N
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int[] my_array = {5, 4, 6, 1, 3, 2, 7, 8, 9, 10};
+
+        int i = findIndex(my_array, 7);
+        // find the index of 5
+        System.out.println("hello");
+        System.out.println("Index position of 5 is: "
+                + my_array[i]);
+    }
+}
 
